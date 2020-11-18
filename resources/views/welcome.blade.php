c6b8f5da8d45e47b299dd7cafa94bd86ced526c3
dave <david.ifeanyi84@gmail.com>
2020-11-18 08:38:38 +0100

    dave

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -387,9 +387,9 @@
                                         class="tkn-crt-ttl">Presale/Public</span></li>
                                 <li class="tkn-crt-itm"><span class="tkn-crt-prcnt six"><span>40%</span></span><span
                                         class="tkn-crt-ttl">Locked up supply</span></li>
-                                <li class="tkn-crt-itm">
-                                    {{-- <span class="tkn-crt-prcnt seven"><span>4                                                                                                                                                                                                                %</span></span><span --}}
-                                        class="tkn-crt-ttl">TOTAL SUPPLY</span></li>
+                                {{-- <li class="tkn-crt-itm">
+                                    <span class="tkn-crt-prcnt seven"><span>4                                                                                                                                                                                                                %</span></span><span
+                                        class="tkn-crt-ttl">TOTAL SUPPLY</span></li> --}}
                             </ul>
                         </div>
                     </div><!-- End tab-pane -->
